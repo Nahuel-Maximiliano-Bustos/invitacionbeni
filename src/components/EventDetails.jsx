cc86c0551e4e5e5b460dc5b40b21a17664912640
Nahuel-Maximiliano-Bustos <nahuel.m.bustos@gmail.com>
2026-09-13 16:01:15 -0300

    Update event date and time to Sept 19, 16:00 to 19:00

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -14,7 +14,7 @@ const EventDetails = () => {
             <div className="flex flex-col items-center">
               <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" className="text-5xl text-blue-500 mb-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zm-6 400H54c-3.3 0-6-2.7-6-6V160h352v298c0 3.3-2.7 6-6 6z"></path></svg>
               <h3 className="text-2xl font-bold text-white">FECHA ESTELAR</h3>
-              <p className="text-xl text-cyan-300 mt-2">5 SEPTIEMBRE</p>
+              <p className="text-xl text-cyan-300 mt-2">19 SEPTIEMBRE</p>
             </div>
             
             <div className="hidden md:block w-px h-32 bg-gradient-to-b from-transparent via-cyan-500 to-transparent"></div>
